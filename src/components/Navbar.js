665556db27af66c40804a3d7acbe0e15a51f53af
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-10-11 11:44:28 +0100

    Installed react styled component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+	height: 60px;
+`
 
 const Navbar = () => {
 	return (
-		<div>
+		<Container>
 			 navbar
-		</div>
+		</Container>
 	)
 }
 
