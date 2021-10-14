20e403c5cbb3c624245161e5340b8908dba723c5
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-10-14 15:47:39 +0100

    Create dat for slides

---
+++ src/components/data.js
@@ -0,0 +1,25 @@
+import style from "../image/style.png"
+
+export const slideritems = [
+	{
+		id: 1,
+		img: style,
+		title: "Summer sale",
+		desc:  "Lorem ipsum gihjhi9 uktynioyibj tiuobntoyby iubnyoy",
+		bg: "f5fafd",
+	},
+	{
+		id: 2,
+		img: style,
+		title: "Summer sale",
+		desc:  "frrtyyt gihjhi9 uktynioyibj tiuobntoyby iubnyoy",
+		bg: "fcf1ed",
+	},
+	{
+		id: 3,
+		img: style,
+		title: "Summer sale",
+		desc:  "Lorem ipsum gihjhi9 uktynioyibj tiuobntoyby iubnyoy",
+		bg: "fbf0f4",
+	},
+]
