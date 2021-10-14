6865160afd32ef0b85787b536cd4f1a05e75d26b
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-10-14 16:04:52 +0100

    Add transition effect

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -32,7 +32,7 @@ const Arrow = styled.div`
 	const Wrapper = styled.div`
 	height: 100%;
 	display: flex;
-	transition: 
+	transition: all 1.5s ease;
 	transform: translateX(${props => props.slideIndex * -100}vw);
 `;
 
