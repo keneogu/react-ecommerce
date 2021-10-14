ce2da334f93ec5df3cf5f5127145b7789d07e7a7
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-10-14 15:48:49 +0100

    Add slider codes which slides when you click the arrow icons

---
+++ src/components/Slider.js
@@ -0,0 +1,100 @@
+import { ArrowLeftOutlined, ArrowRightOutlined } from '@material-ui/icons';
+import React, { useState } from 'react'
+import styled from "styled-components";
+import { slideritems } from "./data";
+
+const Container = styled.div`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	position: relative;
+	overflow: hidden;
+`
+const Arrow = styled.div`
+	width: 50px;
+	height: 50px;
+	background-color: white;
+	border-radius:  50%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	margin: auto;
+	left: ${props=> props.direction === "left" && "10px" };
+  right: ${props=> props.direction === "right" && "10px" };
+	cursor: pointer;
+	opaque: 0.5;
+	z-index: 2;
+	`;
+
+	const Wrapper = styled.div`
+	height: 100%;
+	display: flex;
+	transition: 
+	transform: translateX(${props => props.slideIndex * -100}vw);
+`;
+
+const Slide = styled.div`
+	width:100vw;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+  background-color: #${props=> props.bg};
+`
+
+const ImgContainer = styled.div`
+  height: 100%;
+  flex: 1;
+`
+
+const InfoContainer = styled.div`
+	flex: 1;
+	padding: 50px;
+`
+
+const Image = styled.img`
+  height: 80%; 
+`
+
+const Title = styled.h1``
+const Desc = styled.p``
+const Button = styled.button``
+
+const Slider = () => {
+	const [slideIndex, setSlideIndex] = useState(0)
+	const handleClick = (direction) => {
+    if (direction === "left") {
+      setSlideIndex(slideIndex > 0 ? slideIndex-1 : 2)
+		}  else {
+			setSlideIndex(slideIndex < 2 ? slideIndex +1 : 0)
+		}
+	}
+	return (
+		<Container>
+			<Arrow direction="left" onClick={() => handleClick("left")}>
+				<ArrowLeftOutlined />
+			</Arrow>
+			<Wrapper slideIndex={slideIndex}>
+				{slideritems.map((item) => (
+					<Slide bg={item.bg}>
+				<ImgContainer>
+				  <Image src={item.img} alt="fashion" />
+				</ImgContainer>
+				<InfoContainer>
+					<Title>{item.title}</Title>
+					<Desc>{item.desc}</Desc>
+					<Button>SHOW NOW</Button>
+				</InfoContainer>
+				</Slide>
+				))}
+			</Wrapper>
+			<Arrow direction="right" onClick={() => handleClick("right")}>
+				<ArrowRightOutlined />
+			</Arrow>
+		</Container>
+	)
+}
+
+export default Slider
